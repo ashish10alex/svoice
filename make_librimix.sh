320a2af422cbbff0988ba221121c68a38ae7f3cc
aaa18-txk02 <ashish.alex10@gmail.com>
2020-12-18 02:03:17 +0000

    make code consise

--- make_librimix.sh
+++ make_librimix.sh
@@ -6,6 +6,8 @@
 # LICENSE file in the root directory of this source tree.
 # Author: Yossi Adi (adiyoss)
 
+base_path=/jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/
+
 tr_path=egs/librimix_dataset/tr
 val_path=egs/librimix_dataset/vl
 test_path=egs/librimix_dataset/tt
@@ -26,15 +28,15 @@ fi
 
 
 
-#python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/train-100/mix_both > $tr_path/mix.json
-#python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/train-100/s1 > $tr_path/s1.json
-#python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/train-100/s2 > $tr_path/s2.json
+#python -m svoice.data.audio $base_path/train-100/mix_both > $tr_path/mix.json
+#python -m svoice.data.audio $base_path/train-100/s1 > $tr_path/s1.json
+#python -m svoice.data.audio $base_path/train-100/s2 > $tr_path/s2.json
 
 
-python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/dev/mix_both > $val_path/mix.json
-python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/dev/s1 > $val_path/s1.json
-python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/dev/s2 > $val_path/s2.json
+#python -m svoice.data.audio $base_path/dev/mix_both > $val_path/mix.json
+#python -m svoice.data.audio $base_path/dev/s1 > $val_path/s1.json
+#python -m svoice.data.audio $base_path/dev/s2 > $val_path/s2.json
 
-python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/test/mix_both > $test_path/mix.json
-python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/test/s1 > $test_path/s1.json
-python -m svoice.data.audio /jmain01/home/JAD007/txk02/aaa18-txk02/Datasets/asteroid/egs/librimix/ConvTasNet/data/Libri2Mix/wav8k/min/test/s2 > $test_path/s2.json
+#python -m svoice.data.audio $base_path/test/mix_both > $test_path/mix.json
+#python -m svoice.data.audio $base_path/test/s1 > $test_path/s1.json
+#python -m svoice.data.audio $base_path/test/s2 > $test_path/s2.json
